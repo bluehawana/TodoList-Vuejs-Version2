1d100b1758ecd963d807e7d42045ed826a6178e5
Hongzhi Li <bluehawana@gmail.com>
2022-09-23 11:53:20 +0200

    new features with id order, status and so on

--- src/components/Todolist.vue
+++ src/components/Todolist.vue
@@ -1,80 +1,105 @@
 <template>
   <div class="container">
+    <h1 class="text-center">My Todolist</h1>
 
-      <h1 class="text-center ">My Todolist</h1>
-
-  <div class="d-flex">
-<input v-model ="todo" type="text" placeholder="Enter your todos here" class="form-control">
-<button @click="savingTodo" class="btn btn-primary rounded-2">Saving</button>  
-</div>
-<div style="margin-top: 25px">    </div>
-<table class="table table-striped table-secondary">
-  <thead>
-    <tr >
-      <th scope="col">Order</th>
-      <th scope="col">Todos</th>
-      <th scope="col">Status</th>
-      <th scope="col" class="text-center">Editing</th>
-      <th scope="col" class="text-center">Delete</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr v-for="(todo,key,index) in todos" :key="index" >
-      <td>{{key}}</td>
-      <td>{{todo.name}}</td>
-      <td>{{todo.status}}</td>
-      <td>
-        <div class="text-center">
-          <span class="fa fa-edit"></span>
-        </div>
-      </td>
-      <td><div class="text-center" @click="deleteTodo(index)">
-          <span class="fa fa-trash-can"></span>
-        </div></td>
-    </tr>
-  </tbody>
-</table>
+    <div class="d-flex">
+      <input
+        v-model="todo"
+        type="text"
+        placeholder="Enter your todos here"
+        class="form-control"
+      />
+      <button @click="savingTodo" class="btn btn-primary rounded-2">
+        Saving
+      </button>
+    </div>
+    <div style="margin-top: 25px"></div>
+    <table class="table table-striped table-secondary">
+      <thead>
+        <tr>
+          <th scope="col">Order</th>
+          <th scope="col">Todos</th>
+          <th scope="col">Status</th>
+          <th scope="col" class="text-center">Editing</th>
+          <th scope="col" class="text-center">Delete</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(todo, id) in todos" :key="id">
+          <td>{{ id }}</td>
+          <td>{{ todo.name }}</td>
+          <td>
+            <span @click="changeStatus(id)" class="pointer">
+              {{ todo.status }}
+            </span>
+          </td>
+          <td class="text-center">
+            <div @click="editTodo(id)">
+              <p class="fa fa-edit"></p>
+            </div>
+          </td>
+          <td>
+            <div class="text-center" @click="deleteTodo(id)">
+              <span class="fa fa-trash-can"></span>
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
-  
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
-    msg: String
+    msg: String,
   },
   data() {
     return {
-      todo:'hello works 785965 ',
-      todos: [
-        {
-          name: 'Play padel with kris.',
-          status: 'Todo'
+      todo: " ",
+      editedTodo: null,
+      availability: ["ToDo", "Doing", "Done"],
 
-        },
-        {
-          name: 'Play padel with kris.',
-          status: 'Todo'
+      todos: [],
+    };
+  },
+  methods: {
+    savingTodo() {
+      if (this.todo.length === 0) return;
 
-        }
-      ]
-    }
- },
-      methods: {
-      savingTodo(){
-          if (this.todo.length === 0) return;
+      if (this.editedTodo === null) {
+        this.todos.push({
+          name: this.todo,
+          status: "ToDo",
+        });
+      } else {
+        this.todos[this.editedTodo].name = this.todo;
+        this.editedTodo = null;
+      }
 
-          this.todos.push({
-          
-            name: this.todo,
-            status:'ToDo'
-          })
-        },
-        deleteTodo(index) {
-          this.todos.splice(index, 1);
-  }
-}
- 
+      this.todo = "";
+    },
+
+    deleteTodo(id) {
+      this.todos.splice(id, 1);
+    },
+
+    editTodo(id) {
+      this.todo = this.todos[id].name;
+      this.editedTodo = id;
+    },
+    changeStatus(id) {
+      let newID = this.availability.indexOf(this.todos[id].status);
+      if (++newID > 2) newID = 0;
+      this.todos[id].status = this.availability[newID];
+    },
+  },
 };
 </script>
+
+<style scoped>
+.pointer {
+  cursor: pointer;
+}
+</style>
